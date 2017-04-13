ae20114deb9d8059565310ed4c23dd6997a3224c
Han <hwang031@seattlecentral.edu>
2017-04-12 20:39:13 -0700

    This is for assignment2 Calendar of Events

---
+++ Assignment2/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calendar of Events</title>
+    <link href="StyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Calendar Events</h1>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        <p><asp:Label ID="Label1" runat="server" Text="" CssClass="result"></asp:Label></p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignment2/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[,] events = new string[5,2];
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        events[0,0]= "4/8/2017";
+        events[0, 1] = "Tea Party";
+        events[1, 0] = "4/10/2017";
+        events[1, 1] = "TV Show";
+        events[2, 0] = "4/8/2017";
+        events[2, 1] = "Movie Night";
+        events[3, 0] = "4/16/2017";
+        events[3, 1] = "Chinese Food Night";
+        events[4, 0] = "4/23/2017";
+        events[4, 1] = "Japanese Movie Night";
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        string myDate = Calendar1.SelectedDate.ToShortDateString();
+
+        Label1.Text = "";
+
+        for(int i = 0; i < 5; i++)
+        {
+            if (myDate.Equals(events[i,0]))
+            {
+                Label1.Text += events[i, 1] + "<br />";
+            }
+        }
+
+    }
+}
