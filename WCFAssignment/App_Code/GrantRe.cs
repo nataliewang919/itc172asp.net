e35c2d6c743c1084fc3502937c7f6b0cc4f64a37
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-05-16 12:21:57 -0700

    this is for wcf assignment

---
+++ WCFAssignment/App_Code/GrantRe.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GrantRe" in code, svc and config file together.
+public class GrantRe : IGrantRe
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public List<GrantRequestInfo> GetRequestGrantAmount(int Key)
+    {
+        var grant = from g in db.GrantRequests
+                    from r in g.GrantReviews
+                    where g.PersonKey == Key
+                    select new { g.PersonKey, g.GrantRequestDate, g.GrantRequestAmount, g.GrantRequestExplanation,g.GrantTypeKey,
+                        r.GrantRequestStatus };
+
+        List<GrantRequestInfo> GRequest = new List<GrantRequestInfo>();
+
+        foreach(var gr in grant)
+        {
+            GrantRequestInfo gr1 = new GrantRequestInfo();
+            gr1.GrantRequestDate = gr.GrantRequestDate.ToShortDateString();
+            gr1.GrantRequestExplanation = gr.GrantRequestExplanation;
+            gr1.GrantRequestAmount = gr.GrantRequestAmount.ToString();
+            gr1.GrantTypeKey = (int)gr.GrantTypeKey;
+            gr1.PersonKey = (int)gr.PersonKey;
+
+            gr1.GrantRequestStatus = gr.GrantRequestStatus;
+            GRequest.Add(gr1);
+            
+        }
+        return GRequest;
+    }
+
+    public int Login(string user, string password)
+    {
+        int key = 0;
+        int result = db.usp_Login(user, password);
+        if (result != -1)
+        {
+            var userKey = (from k in db.People
+                           where k.PersonEmail.Equals(user)
+                           select k.PersonKey).FirstOrDefault();
+            key = (int)userKey;
+        }
+        return result;
+    }
+
+    public bool newGrant(GrantRequest r)
+    {
+        bool result = true;
+        try { 
+        db.GrantRequests.Add(r);
+        db.SaveChanges();
+        }catch {
+            result = false;
+        }
+        return result;
+    }
+
+    public bool RegisterUser(string lastname, string firstname, string email, string password, string Apartment, string Street, string City, string State, string Zipcode, string HomePhone, string WorkPhone)
+    {
+        bool result = true;
+        int rev = db.usp_Register(lastname, firstname, email, password, Apartment,
+            Street, City, State, Zipcode, HomePhone, WorkPhone);
+
+        return result;
+    }
+}
---
+++ WCFAssignment/App_Code/IGrantRe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGrantRe" in both code and config file together.
+[ServiceContract]
+public interface IGrantRe
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterUser(string lastname, string firstname, string email, string password,
+    string Apartment, string Street, string City, string State, string Zipcode,
+    string HomePhone, string WorkPhone);
+
+    [OperationContract]
+    List<GrantRequestInfo> GetRequestGrantAmount(int Key);
+
+    [OperationContract]
+    bool newGrant(GrantRequest r);
+}
+
+[DataContract]
+public class GrantRequestInfo
+
+{
+    [DataMember]
+    public string GrantRequestDate { get; set; }
+
+    [DataMember]
+    public string GrantRequestExplanation { get; set; }
+
+    [DataMember]
+    public string GrantRequestAmount { get; set; }
+
+    [DataMember]
+    public string GrantRequestStatus { get; set; }
+
+    [DataMember]
+    public int PersonKey { get; set; }
+
+    [DataMember]
+    public int GrantTypeKey { get; set; }
+}
