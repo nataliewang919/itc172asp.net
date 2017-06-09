74692d12f238c07638171a52a5a056a846b9502a
nataliewang919 <nataliewang919@kakoshaochies-MacBook-Pro.local>
2017-06-09 11:36:49 -0700

    This is for final project

---
+++ FinalAssignment/Controllers/GrantRequestInfoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalAssignment.Models;
+
+namespace FinalAssignment.Controllers
+{
+    
+        public class GrantRequestInfoController : Controller
+        {
+            Community_AssistEntities db = new Community_AssistEntities();
+            // GET: GrantRequestInfo
+            public ActionResult Index()
+            {
+                var per = from g in db.GrantRequests
+                          from r in db.GrantReviews
+                          select new
+                          {
+                              g.GrantRequestDate,
+                              g.GrantRequestExplanation,
+                              g.GrantRequestAmount,
+                              r.GrantReviewDate,
+                              r.GrantRequestStatus,
+                              r.GrantAllocationAmount
+                          };
+                List<GrantRequestInfo> gr = new List<GrantRequestInfo>();
+
+                foreach (var pers in per)
+                {
+                    GrantRequestInfo gre = new GrantRequestInfo();
+                    gre.GrantRequestDate = pers.GrantRequestDate.ToString();
+                    gre.GrantRequestAmount = pers.GrantRequestAmount.ToString();
+                    gre.GrantRequestExplanation = pers.GrantRequestExplanation;
+                    gre.GrantRequestStatus = pers.GrantRequestStatus;
+                    gre.GrantReviewDate = pers.GrantReviewDate.ToString();
+                    gre.GrantAllocationAmount = pers.GrantAllocationAmount.ToString();
+                    gr.Add(gre);
+                }
+                return View(gr);
+            }
+        }
+    }
---
+++ FinalAssignment/Models/GrantRequestInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalAssignment.Models
+{
+    public class GrantRequestInfo
+    {
+        public string GrantRequestDate { get; set; }
+        public string GrantRequestExplanation { get; set; }
+        public string GrantRequestAmount { get; set; }
+        public string GrantRequestStatus { get; set; }
+        public string GrantAllocationAmount { get; set; }
+        public string GrantReviewDate { get; set; }
+    }
+}
